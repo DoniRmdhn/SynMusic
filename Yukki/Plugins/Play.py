95451b33fe7cc2dd6c8f968565ccf9f5493c290f
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-01 17:03:48 +0530

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -51,7 +51,7 @@ FSUBB = InlineKeyboardMarkup(
 
 
 @app.on_message(
-    filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group
+    filters.command(["mplay", f"mplay@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
