7a5c09feb1ccfaa39a5f599d12a4cc4922c85230
szsupunma <86388269+szsupunma@users.noreply.github.com>
2022-03-02 16:07:46 -0800

    Done

--- Yukki/Plugins/Ping.py
+++ Yukki/Plugins/Ping.py
@@ -9,12 +9,6 @@ from pyrogram.types import Message
 from Yukki import BOT_USERNAME, MUSIC_BOT_NAME, app, boottime
 from Yukki.Utilities.ping import get_readable_time
 
-__MODULE__ = "Ping"
-__HELP__ = """
-
-/ping - Check if Bot is alive or not.
-"""
-
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - boottime)
@@ -22,23 +16,23 @@ async def bot_sys_stats():
     mem = psutil.virtual_memory().percent
     disk = psutil.disk_usage("/").percent
     stats = f"""
-Uptime: {get_readable_time((bot_uptime))}
-CPU: {cpu}%
-RAM: {mem}%
-Disk: {disk}%"""
+- Uptime: {get_readable_time((bot_uptime))}
+- CPU: {cpu}%
+- RAM: {mem}%
+- Disk: {disk}%"""
     return stats
 
 
 @app.on_message(filters.command(["ping", f"ping@{BOT_USERNAME}"]))
 async def ping(_, message):
     start = datetime.now()
-    response = await message.reply_photo(
-        photo="Utils/Query.jpg",
-        caption=">> Pong!",
-    )
+    response = await message.reply_text("☬ `Pong!`")
     uptime = await bot_sys_stats()
     end = datetime.now()
     resp = (end - start).microseconds / 1000
     await response.edit_text(
-        f"**Pong!**\n`⚡{resp} ms`\n\n<b><u>{MUSIC_BOT_NAME} System Stats:</u></b>{uptime}"
+        f"""
+ - **Pong!**`{resp} ms`
+ <b><u>{MUSIC_BOT_NAME} System Stats: </u></b>
+ {uptime}"""
     )
