ec4a45c03adb4cbda7d388c20da22c278117f968
szsupunma <86388269+szsupunma@users.noreply.github.com>
2022-03-02 18:13:44 -0800

    await message.reply_text

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -179,7 +179,7 @@ async def play(_, message: Message):
             buttons = playlist_markup(
                 message.from_user.first_name, message.from_user.id, "abcd"
             )
-            await message.reply_photo(
+            await message.reply_text(
                     "**Usage:** /play [Music Name or Youtube Link or Reply to Audio]\n\nIf you want to play Playlists! Select the one from Below.",
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
