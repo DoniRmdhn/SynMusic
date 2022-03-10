b06ad084c0791f6aa81156e31fb6ee4c19f057c3
Aman Vishwakarma <95975318+AMANTYA1@users.noreply.github.com>
2022-03-11 01:39:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = "https://github.com/szsupunma/Rose-music"
+UPSTREAM_REPO = "https://github.com/AMANTYA1/Le-baji"
 UPSTREAM_BRANCH = "main"
 
 SUPPORT_CHANNEL = "https://t.me/slbotzone"
