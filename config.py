db3394f652d9e413b9e7ce484e402dd98a8c2397
DoniRmdhn <103444731+DoniRmdhn@users.noreply.github.com>
2022-04-14 04:35:32 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ get_queue = {}
 BOT_TOKEN = getenv("BOT_TOKEN")
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "10"))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "160"))
 ASSISTANT_PREFIX = list(getenv("ASSISTANT_PREFIX", ".").split())
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
@@ -20,11 +20,11 @@ MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
-UPSTREAM_REPO = "https://github.com/AMANTYA1/Rose-music"
+UPSTREAM_REPO = "https://github.com/DoniRmdhn/SynMusic"
 UPSTREAM_BRANCH = "main"
 
-SUPPORT_CHANNEL = "https://t.me/slbotzone"
-SUPPORT_GROUP = "https://t.me/szteambots"
+SUPPORT_CHANNEL = "https://t.me/synxupdate"
+SUPPORT_GROUP = "https://t.me/synxsupport"
 
 THUMBNAIL = getenv("THUMB_LINK") 
 
